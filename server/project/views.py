65d46e9cbb487ca8a1b59856e35d9ea31f7ad4d0
bewakes <spirit_bibek@yahoo.com>
2018-05-20 17:24:38 +0545

    add filter projects by user_group

--- server/project/views.py
+++ server/project/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
 from chrono.permissions import ModifyPermission
 from .models import Project
@@ -13,6 +14,8 @@ class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAuthenticated,
                           ModifyPermission]
+    filter_classes = [DjangoFilterBackend]
+    filter_fields = ['user_group']
 
     def get_queryset(self):
         return Project.get_for(self.request.user)
