ef4494f0e5cc618bb0b7b28ba73e13ad3613ebe1
bewakes <bewakepandey@gmail.com>
2020-04-20 10:54:37 +0545

    Fix page size to 1000 instead of 10000

--- server/chrono/settings.py
+++ server/chrono/settings.py
@@ -129,7 +129,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10000,
+    'PAGE_SIZE': 1000,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
